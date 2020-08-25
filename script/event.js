2f156f3536e6c1427b961659909387fb3a3b83b1
sugar2456 <sugar2456_0526@yahoo.co.jp>
2020-08-25 09:42:25 +0900

    ひまわり、ゆり追加

--- script/event.js
+++ script/event.js
@@ -6,21 +6,20 @@
 function OnButtonClick(id) {
     createArticle(id);
 }
+
 function createArticle(id) {
     const article_id = id.split('_')[0];
     const mainElement = document.getElementById("main_id");
     while(mainElement.firstChild) {
         mainElement.removeChild(mainElement.firstChild);
     }
-    let articleElement = document.createElement("article");
-    let addHeader = document.createElement("h1");
-    addHeader.appendChild(document.createTextNode("header"));
-    // addHeader
-    articleElement.appendChild(addHeader);
-    articleElement.appendChild(document.createTextNode(article_id));
-    articleElement.className = "main-article-show";
-    mainElement.appendChild(articleElement);
+    let iframeElement = document.createElement("iframe");
+    const path = "./article-" + article_id + ".html";
+    iframeElement.src = path;
+    iframeElement.className = "main-iframe-style";
+    mainElement.appendChild(iframeElement);
 }
+
 const buttons = document.querySelectorAll('button');
 buttons.forEach(function(button) {
     button.addEventListener('click', function() {OnButtonClick(button.id)});
